94e961acc320a6714c092cf39c5094d30d970088
Siju Xavier <siju.xavier@ucalgary.ca>
2026-03-20 17:25:23 -0600

    feat: Introduce animated fire effects with `FireSource`, `FireResponder`, and `SpriteMover` components.

---
+++ Assets/Scripts/Presentation/FireSource.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Presentation
+{
+    public class FireSource : MonoBehaviour
+    {
+        // Simple tag component to mark an object as a fire
+        // Can be expanded later with fire intensity, etc.
+    }
+}
